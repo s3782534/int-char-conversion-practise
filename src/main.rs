6ffbf87e3da34b84b4fdc8420ad379bab57c9150
Jordan <jrsorrenti@gmail.com>
2022-01-02 11:30:54 +1100

    Added exit to main menu, implemented character to integer

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         println!("Which mode would you like to use:");
         println!("1. Integer to Character");
         println!("2. Character to integer");
+        println!("0. Exit");
         println!("Input the number to enter that mode.");
 
         io::stdin()
@@ -18,6 +19,8 @@ fn main() {
             int_to_char();
         } else if mode == 2 {
             char_to_int();
+        } else if mode == 0 {
+            break;
         }
     }
 }
@@ -28,4 +31,10 @@ fn int_to_char() {
 
 fn char_to_int() {
     println!("Enter character to convert to an integer:");
+    let mut char_input = String::new();
+    io::stdin()
+        .read_line(&mut char_input)
+        .expect("Failed to read line");
+    let c: char = char_input.trim().parse().expect("Please type a character");
+    println!("{} is represented as {} as a decimal integer.", c, c as u32);
 }
